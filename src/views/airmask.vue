7f40ab3d75faec52d7004bc5b535a9c655e02892
高跃 <kouyue@kouyuedeiMac.local>
2020-07-06 00:41:35 +0900

    20200705

--- src/views/airmask.vue
+++ src/views/airmask.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="airmsk">
+    <div class="airmask">
 <span>
-    <div class="airmaskimg1 airP"></div>
+    <div class="airmaskimg1 airP anow">
+        <div class="airmaskimg2 alater"></div>
+    </div>
+    <div class="airback airP">
+        <div class="airbackin"></div>
+        <div class="airoutinfo">
+        <div class=" airtitle">{{ this.$t('airmask.airmasktitle1')}}</div>    
+        <div class="airinfo1" v-html="this.$t('airmask.airmaskinfo1').replace(/\n/g,'<br\>')"></div>
+        </div>
+     </div>
+    
 </span>
     </div>
 </template>
@@ -16,7 +26,18 @@ export default {
     position: absolute;
     background-size: cover;
 }
+.anow{
+    transition: 0.6s;
+}
+.alater{
+    transition: all 0.6s ease-out 0.4s;
+}
 @media screen and(min-width: 500px){
+    .airtitle{
+       font-size: 2rem;
+       margin-bottom: 25px;
+       color: white;
+    }
     .airmask{
     position: relative;
     max-width: 1280px;
@@ -32,9 +53,45 @@ export default {
     color: #0b0b0b;
         .airmaskimg1{
             background-image: url(../200624_0011-1.png);
-            width: 600px;
-            height: 600px;
+            width: 1280px;
+            height: 1030px;
+            transition: 0.6s;
+            .airmaskimg2{
+                background-image:url(../200624_0010.png);
+                width: 500px;
+                height: 900px;
+                background-size: contain;
+                margin: auto;
+            }
         }
-    }
-}
+               .airback{
+                background-color: #42c7fc;
+                width: 1280px;
+                height: 2000px;
+                z-index: -1;
+                top:800px;
+                .airbackin{
+                        width: 1500px;
+                        height: 1500px;
+                        top: -1140px;
+                        background-color: white;
+                        border-radius: 800px;
+                        position: absolute;
+                        left: -110px;
+                        box-shadow: 0 0 12px 0px #00000038;
+                        }
+                        .airoutinfo{
+                           margin: 450px auto auto auto;
+                           text-align: center;
+                                .airinfo1{
+                                    margin: auto;
+                                    text-align: center;
+                                    color: white;
+                                    width: 60%;
+                                }
+                            }
+                     }
+            }
+    
+}       
 </style>
